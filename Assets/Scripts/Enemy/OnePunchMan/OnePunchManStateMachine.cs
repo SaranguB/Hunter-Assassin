56622a9ed5718148f2d523385fa05f465d8ac15b
Sarangu Baisil Sanal <163407247+SaranguB@users.noreply.github.com>
2025-03-01 14:21:35 +0530

    GenericStateMachine

--- Assets/Scripts/Enemy/OnePunchMan/OnePunchManStateMachine.cs
+++ Assets/Scripts/Enemy/OnePunchMan/OnePunchManStateMachine.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace StatePattern.Enemy
 {
-    public class OnePunchManStateMachine : IStateMachine
+    public class OnePunchManStateMachine : GenericStateMachine
     {
         private OnePunchManController Owner;
         private IState currentState;
